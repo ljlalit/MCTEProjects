0061e557a0af5769fcc7bf294afc6825d803703a
tanmaydhama <59246744+tanmaydhama@users.noreply.github.com>
2020-10-06 23:24:22 +0530

    fixes in adminhome

--- lib/adminhome.dart
+++ lib/adminhome.dart
@@ -1,3 +1,4 @@
+import 'package:QRhelp/RedButton.dart';
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
@@ -7,11 +8,12 @@ class adminhome extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffffffff),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Text.rich(
             TextSpan(
               style: TextStyle(
-                fontFamily: 'NeueKabelW01-Regular',
+                fontFamily: 'NeueKabel',
                 fontSize: 48,
                 color: const Color(0xff000000),
               ),
@@ -22,68 +24,23 @@ class adminhome extends StatelessWidget {
                 TextSpan(
                   text: 'Admin',
                   style: TextStyle(
-                    fontFamily: 'NeueKabelW01-Bold',
+                    fontFamily: 'NeueKabel',
                   ),
                 ),
               ],
             ),
             textAlign: TextAlign.left,
           ),
-          Container(
-            width: 130.0,
-            height: 55.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: const Color(0xffe53935),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0xff000000),
-                  offset: Offset(0, 3),
-                  blurRadius: 6,
-                ),
-              ],
-            ),
+          Image(
+            image: AssetImage('assets/Signals (1).png'),
           ),
-          SizedBox(
-            width: 71.0,
-            child: Text(
-              'Scan',
-              style: TextStyle(
-                fontFamily: 'NeueKabelW01-Regular',
-                fontSize: 29,
-                color: const Color(0xffffffff),
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Container(
-            width: 47.0,
-            height: 36.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(3.0),
-              image: DecorationImage(
-                image: const AssetImage(''),
-                fit: BoxFit.fill,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0x91000000),
-                  offset: Offset(3, 3),
-                  blurRadius: 6,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: 357.0,
-            height: 357.0,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: const AssetImage(''),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
+          RedButton(
+            text: 'Scan',
+            width: 200.0,
+            height: 20.0,
+            onPressed: () {},
+            c: Colors.redAccent,
+          )
         ],
       ),
     );
