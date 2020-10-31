453d95688b4dac3cbd2a62ce97fad4eb9814abca
siddharthkatyal <54847921+siddharthkatyal@users.noreply.github.com>
2020-10-31 21:59:21 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 // ignore: camel_case_types
 class home extends StatefulWidget {
@@ -26,7 +27,7 @@ class _homeState extends State<home> {
     getCurrentUser();
   }
 
-  String name = 'loading';
+  String name;
   String type;
   var docref;
   var data;
@@ -37,173 +38,203 @@ class _homeState extends State<home> {
       final user = await _auth.currentUser;
       if (user != null) {
         loggedinUser = user;
-        docref = _firestore.collection("users").doc(loggedinUser.uid);
-        docref.get().then((value) {
-          if (value.exists) {
-            data = value.data();
-            setState(() {
-              this.name = data['Name'];
-              this.type = data['Type'];
-              if (type == 'admin') {
-                Navigator.pushNamed(context, 'adminhome');
-              }
-            });
-          }
-        }).catchError((e) {
-          print(e);
-        });
       }
     } catch (e) {
       print(e);
     }
   }
 
+  Future<Userdata> getCurrentUserData() {
+    docref = _firestore.collection("users").doc(loggedinUser.uid);
+    docref.get().then((value) {
+      if (value.exists) {
+        data = value.data();
+        Userdata userdata = Userdata(
+            data["Email"],
+            data["Name"],
+            data["Number"],
+            data["Rank"],
+            data["Services"],
+            data["Type"],
+            data["Unit"]);
+        return userdata;
+      }
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.redAccent,
-            ),
-            drawer: Theme(
-              data: Theme.of(context).copyWith(
-                canvasColor: Colors.redAccent,
-              ),
-              child: Drawer(
-                child: ListView(
-                  children: <Widget>[
-                    ListTile(
-                      title: SizedBox(),
-                    ),
-                    ListTile(
-                      onTap: () => {Navigator.pushNamed(context, 'profile')},
-                      title: Text(
-                        'Your Profile',
-                        style: TextStyle(
-                          fontFamily: 'NeueKabel',
-                          fontSize: 32,
-                          color: const Color(0xffffffff),
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Divider(),
-                    ListTile(
-                      onTap: () => ({Navigator.pushNamed(context, 'services')}),
-                      title: Text(
-                        'Your Services',
-                        style: TextStyle(
-                          fontFamily: 'NeueKabel',
-                          fontSize: 32,
-                          color: const Color(0xffffffff),
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Divider(),
-                    ListTile(
-                        onTap: () => ({logout(context)}),
-                        title: SizedBox(
-                          width: 118.0,
-                          child: Text(
-                            'Logout',
-                            style: TextStyle(
-                              fontFamily: 'NeueKabel',
-                              fontSize: 32,
-                              color: const Color(0xffffffff),
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        )),
-                    ListTile(
-                      title: SizedBox(),
-                    ),
-                    ListTile(
-                        title: Container(
-                      width: 300.0,
-                      height: 300.0,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: const AssetImage('assets/Signals (1).png'),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    )),
-                  ],
-                ),
-              ),
-            ),
-            body: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  SizedBox(
-                    height: 0.1,
+        child: FutureBuilder<Userdata>(
+      future: getCurrentUserData(),
+      builder: (BuildContext context, AsyncSnapshot<Userdata> snapshot) {
+        if (snapshot.data == null) {
+          return Container(child: Center(child: Text("Loading...")));
+        } else {
+          return Container(
+              child: Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.redAccent,
                   ),
-                  Text.rich(
-                    TextSpan(
-                      style: TextStyle(
-                        fontFamily: 'NeueKabel',
-                        fontSize: 48,
-                        color: const Color(0xff000000),
-                      ),
-                      children: [
-                        TextSpan(
-                          text: 'Hello, ',
-                        ),
-                        TextSpan(
-                          text: '${name}',
-                          style: TextStyle(
-                            fontFamily: 'NeueKabel',
-                          ),
-                        ),
-                      ],
+                  drawer: Theme(
+                    data: Theme.of(context).copyWith(
+                      canvasColor: Colors.redAccent,
                     ),
-                    textAlign: TextAlign.left,
-                  ),
-                  SizedBox(),
-                  InkWell(
-                    onTap: () => ({
-                      Navigator.pushNamed(
-                        context,
-                        'homeqr',
-                      ),
-                    }),
-                    child: Container(
-                      width: MediaQuery.of(context).copyWith().size.width,
-                      height: 180.0,
-                      // MediaQuery.of(context).copyWith().size.height / 3.5,
-                      decoration: BoxDecoration(
-                        color: Colors.redAccent,
-                      ),
-                      child: Column(
+                    child: Drawer(
+                      child: ListView(
                         children: <Widget>[
-                          Container(
-                            width: 86.0,
-                            height: 86.0,
+                          ListTile(
+                            title: SizedBox(),
+                          ),
+                          ListTile(
+                            onTap: () =>
+                                {Navigator.pushNamed(context, 'profile')},
+                            title: Text(
+                              'Your Profile',
+                              style: TextStyle(
+                                fontFamily: 'NeueKabel',
+                                fontSize: 32,
+                                color: const Color(0xffffffff),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Divider(),
+                          ListTile(
+                            onTap: () =>
+                                ({Navigator.pushNamed(context, 'services')}),
+                            title: Text(
+                              'Your Services',
+                              style: TextStyle(
+                                fontFamily: 'NeueKabel',
+                                fontSize: 32,
+                                color: const Color(0xffffffff),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Divider(),
+                          ListTile(
+                              onTap: () => ({logout(context)}),
+                              title: SizedBox(
+                                width: 118.0,
+                                child: Text(
+                                  'Logout',
+                                  style: TextStyle(
+                                    fontFamily: 'NeueKabel',
+                                    fontSize: 32,
+                                    color: const Color(0xffffffff),
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              )),
+                          ListTile(
+                            title: SizedBox(),
+                          ),
+                          ListTile(
+                              title: Container(
+                            width: 300.0,
+                            height: 300.0,
                             decoration: BoxDecoration(
                               image: DecorationImage(
-                                image: const AssetImage(
-                                    'assets/baseline_keyboard_arrow_up_black_18dp.png'),
+                                image:
+                                    const AssetImage('assets/Signals (1).png'),
                                 fit: BoxFit.fill,
                               ),
                             ),
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            child: Text(
-                              'Your QR',
-                              style: TextStyle(
-                                fontFamily: 'NeueKabel',
-                                fontSize: 48,
-                                color: const Color(0xffffffff),
-                              ),
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
+                          )),
                         ],
                       ),
                     ),
                   ),
-                ])));
+                  body: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 0.1,
+                        ),
+                        Text.rich(
+                          TextSpan(
+                            style: TextStyle(
+                              fontFamily: 'NeueKabel',
+                              fontSize: 48,
+                              color: const Color(0xff000000),
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'Hello, ',
+                              ),
+                              TextSpan(
+                                text: '${snapshot.data.name}',
+                                style: TextStyle(
+                                  fontFamily: 'NeueKabel',
+                                ),
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        SizedBox(),
+                        InkWell(
+                          onTap: () => ({
+                            Navigator.pushNamed(
+                              context,
+                              'homeqr',
+                            ),
+                          }),
+                          child: Container(
+                            width: MediaQuery.of(context).copyWith().size.width,
+                            height: 180.0,
+                            // MediaQuery.of(context).copyWith().size.height / 3.5,
+                            decoration: BoxDecoration(
+                              color: Colors.redAccent,
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  width: 86.0,
+                                  height: 86.0,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: const AssetImage(
+                                          'assets/baseline_keyboard_arrow_up_black_18dp.png'),
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    'Your QR',
+                                    style: TextStyle(
+                                      fontFamily: 'NeueKabel',
+                                      fontSize: 48,
+                                      color: const Color(0xffffffff),
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ])));
+        }
+      },
+    ));
   }
+}
+
+class Userdata {
+  String email;
+  String name;
+  String number;
+  String rank;
+  String type;
+  String unit;
+  List<String> services = [];
+  Userdata(this.email, this.name, this.number, this.rank, this.services,
+      this.type, this.unit);
 }
