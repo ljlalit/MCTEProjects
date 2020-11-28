2a9dddbf1a2773b308d78c72709192135785c1d9
siddharthkatyal <54847921+siddharthkatyal@users.noreply.github.com>
2020-11-29 04:14:00 +0530

    minor errors

--- lib/adminservices.dart
+++ lib/adminservices.dart
@@ -1,10 +1,11 @@
 import 'dart:collection';
-
 import 'package:QRhelp/RedButton.dart';
+import 'package:QRhelp/adminhome.dart';
+import 'package:QRhelp/userservices.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'constants.dart';
 
 // ignore: camel_case_types
 class adminservices extends StatefulWidget {
@@ -31,6 +32,12 @@ class _adminservicesState extends State<adminservices> {
   var servicesdocref;
   int servicesarrlen = 0;
   var servicesarr = [];
+  var servicesarr2 = [];
+  var targetuserdocref;
+  var targetuserdata;
+  var targetusername;
+  var targetuseremail;
+  List<dynamic> targetuserservices;
   LinkedHashMap asd;
   void getCurrentUser() async {
     try {
@@ -42,11 +49,9 @@ class _adminservicesState extends State<adminservices> {
         docref.get().then((value) {
           if (value.exists) {
             data = value.data();
-            setState(() {
-              this.arr = data['Services'];
-              this.arrlen = this.arr.length;
-              getServiceUsers();
-            });
+            this.arr = data['Services'];
+            this.arrlen = this.arr.length;
+            getServiceUsers();
           }
         }).catchError((e) {
           print(e);
@@ -66,11 +71,19 @@ class _adminservicesState extends State<adminservices> {
         setState(() {
           this.asd = servicesdata;
           asd.forEach((key, value) {
+            this.servicesarr2.clear();
+            this.servicesarr.clear();
+            this.servicesarr2.add(key);
             this.servicesarr.add(value["Name"]);
           });
+          this.servicesarrlen = 0;
           this.servicesarrlen = servicesarr.length;
         });
-      } else {}
+      } else {
+        this.servicesarrlen = 0;
+        this.servicesarr2.clear();
+        this.servicesarr.clear();
+      }
     }).catchError((e) {
       print(e);
     });
@@ -103,27 +116,87 @@ class _adminservicesState extends State<adminservices> {
                   textAlign: TextAlign.left,
                 ),
               ]),
+          //if (servicesarrlen != 0)
           for (int i = 0; i < servicesarrlen; i++)
-            if (servicesarrlen != 0)
-              Row(
-                children: [
-                  Text(
-                    '${servicesarr[i]}',
-                    style: TextStyle(
-                      fontFamily: 'Segoe UI',
-                      fontSize: 34,
-                      color: const Color(0xff090909),
-                    ),
-                    textAlign: TextAlign.left,
+            Row(
+              children: [
+                Text(
+                  '${servicesarr[i]}',
+                  style: TextStyle(
+                    fontFamily: 'Segoe UI',
+                    fontSize: 34,
+                    color: const Color(0xff090909),
                   ),
-                  RedButton(
-                      text: 'Unregister',
-                      c: Colors.redAccent,
-                      height: 20.0,
-                      width: 150.0,
-                      onPressed: () {}),
-                ],
-              ),
+                  textAlign: TextAlign.left,
+                ),
+                RedButton(
+                    text: 'Unregister',
+                    c: Colors.redAccent,
+                    height: 20.0,
+                    width: 150.0,
+                    onPressed: () {
+                      _firestore
+                          .collection('servicelist')
+                          .doc(arr[0])
+                          .update({servicesarr2[i]: FieldValue.delete()});
+
+                      targetuserdocref =
+                          _firestore.collection("users").doc(servicesarr2[i]);
+                      targetuserdocref.get().then((value) {
+                        if (value.exists) {
+                          targetuserdata = value.data();
+                          setState(() {
+                            this.targetuserservices =
+                                targetuserdata["Services"];
+                            targetuserservices.remove(arr[0].toString());
+                            try {
+                              _firestore
+                                  .collection('users')
+                                  .doc(servicesarr2[i])
+                                  .update(
+                                {
+                                  'Services': targetuserservices,
+                                },
+                              );
+                            } catch (e) {
+                              print(e);
+                            }
+                          });
+                        }
+                      }).catchError((e) {
+                        print(e);
+                      });
+
+                      print(servicesarr);
+                      print(servicesarr2);
+                      servicesdocref.get().then((value) {
+                        print('Serviczdsxdsddafsfasfes');
+                        if (value.exists) {
+                          print('Servicafsfasfes');
+                          servicesdata = value.data();
+                          setState(() {
+                            asd = servicesdata;
+                            asd.forEach((key, value) {
+                              servicesarr2.clear();
+                              servicesarr.clear();
+                              servicesarr2.add(key);
+                              servicesarr.add(value["Name"]);
+                            });
+                            servicesarrlen = 0;
+                            servicesarrlen = servicesarr.length;
+                          });
+                        } else {
+                          print('Sasfes');
+                          servicesarrlen = 0;
+                          servicesarr2.clear();
+                          servicesarr.clear();
+                        }
+                      }).catchError((e) {
+                        print(e);
+                      });
+                    }),
+              ],
+            ),
 
           //Jo bhi changes krne ho krdiyo
           //services daaliyo font size 34 tho
