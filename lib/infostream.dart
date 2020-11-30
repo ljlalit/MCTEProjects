9a4e7c857e24058a6d571f1b08f8e944275b5621
siddharthkatyal <54847921+siddharthkatyal@users.noreply.github.com>
2020-11-30 17:54:36 +0530

    Update infostream.dart

--- lib/infostream.dart
+++ lib/infostream.dart
@@ -9,6 +9,7 @@ import 'buildTextField.dart';
 // ignore: camel_case_types
 class infostream extends StatefulWidget {
   final String serviceN;
+
   infostream({Key key, this.serviceN}) : super(key: key);
   @override
   _infostreamState createState() => _infostreamState();
@@ -24,41 +25,52 @@ class _infostreamState extends State<infostream> {
   void initState() {
     super.initState();
     serviceName = widget.serviceN;
-    //getCurrentUser();
+    // getCurrentUser();
     getDescription();
   }
 
-  // void getCurrentUser() async {
-  //   try {
-  //     // ignore: await_only_futures
-  //     final user = await _auth.currentUser;
-  //     if (user != null) {
-  //       loggedinUser = user;
-  //       docref = _firestore.collection("users").doc(loggedinUser.uid);
-  //       docref.get().then((value) {
-  //         if (value.exists) {
-  //           data = value.data();
-  //           serviceName = data["Services"][0];
-  //         }
-  //       });
-  //     }
-  //   } catch (e) {
-  //     print(e);
-  //   }
-  // }
-  Future<String> getDescription() async {
-    docref =
-        _firestore.collection('servicedescription').doc(serviceName.toString());
-    data = await docref.get();
-    String desc = data["description"];
-    return desc;
+  void getCurrentUser() async {
+    try {
+      // ignore: await_only_futures
+      final user = await _auth.currentUser;
+      if (user != null) {
+        loggedinUser = user;
+        docref = _firestore.collection("users").doc(loggedinUser.uid);
+        docref.get().then((value) {
+          if (value.exists) {
+            data = value.data();
+            setState(() {
+              serviceName = data["Services"][0];
+            });
+          }
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 
-  String description = '';
+  void getDescription() async {
+    docref =
+        _firestore.collection('servicedescription').doc(serviceName.toString());
+    docref.get().then((value) {
+      if (value.exists) {
+        data = value.data();
+        setState(() {
+          desc = data["description"];
+          print(desc);
+        });
+      }
+    });
+  }
+
+  String desc = '';
+  String description;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+<<<<<<< HEAD
           resizeToAvoidBottomInset: false,
           backgroundColor: Colors.white,
           body: FutureBuilder(
@@ -107,6 +119,49 @@ class _infostreamState extends State<infostream> {
                                   },
                                 );
                               }.then((value) {
+=======
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(
+                child: Container(
+              width: 270,
+              child: TextFormField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 24,
+                initialValue: "${desc.toString()}",
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(width: 7.0, color: Colors.redAccent),
+                  ),
+                  labelText: '$serviceName description',
+                ),
+                onChanged: (value) {
+                  description = value;
+                },
+              ),
+            )),
+            Center(
+              child: RedButton(
+                  text: 'Update',
+                  c: Colors.blueAccent,
+                  onPressed: () {
+                    try {
+                      _firestore
+                          .collection('servicedescription')
+                          .doc(serviceName.toString())
+                          .update(
+                        {
+                          'description': description,
+                        },
+                      ).then((value) {
+>>>>>>> parent of 04a232c... Update infostream.dart
                         Alert(
                           context: context,
                           style: alertStyle,
@@ -127,6 +182,7 @@ class _infostreamState extends State<infostream> {
                           ],
                         ).show();
                       });
+<<<<<<< HEAD
                     }  catch (e) {
                                 print(e);
                               }
@@ -136,6 +192,17 @@ class _infostreamState extends State<infostream> {
                     ],
                   );
               })),
+=======
+                    } catch (e) {
+                      print(e);
+                    }
+                  }),
+            ),
+            SizedBox(),
+          ],
+        ),
+      ),
+>>>>>>> parent of 04a232c... Update infostream.dart
     );
   }
 }
